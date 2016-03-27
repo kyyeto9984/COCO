8ff30b3d09b348d67ad763eace4fd6c66bec479e
kyyeto9984 <kyyeto9984@naver.com>
2016-03-27 19:05:19 +0900

    밍밍

--- app/src/main/java/kr/co/cococ/coco/MainActivity.java
+++ app/src/main/java/kr/co/cococ/coco/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // MING 323
+        // MINGMING
     }
 
     @Override
